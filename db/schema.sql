08cb7618077c77f1b6d03526eac3f241a7e1ae26
pixiepucker <autumnoffic@gmail.com>
2022-04-30 10:50:32 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,30 @@
 DROP DATABASE IF EXISTS employeesDB;
 CREATE DATABASE employeesDB;
 USE employeesDB;
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id)references department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)references roles(id),
+    FOREIGN KEY (manager_id)references employee(id)
+);
