b5762b775fa911e215a03758b0d8ccd287aded4d
pixiepucker <autumnoffic@gmail.com>
2022-04-30 10:57:47 -0500

    updated seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUES
+('finance'),
+('developement');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('manager', 150000, 2),
+('developer', 100000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Max','Storm',1),
+('Autumn','Cain',2,1);
